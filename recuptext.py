3cc719864f09a887b42134b07be31891b4e397db
tanguy85 <tanguy.potier@ynov.com>
2020-01-27 18:42:15 +0100

    recover all pages + hash per 5 pages

---
+++ recuptext.py
@@ -0,0 +1,31 @@
+import PyPDF2, hashlib, math
+
+pdf_file = open('INFO_Maupassant_Bel_Ami.pdf', 'rb')
+read_pdf = PyPDF2.PdfFileReader(pdf_file)
+
+count = 0
+block = ''
+
+for x in range (read_pdf.numPages):
+
+    content = read_pdf.getPage(x)
+    text = content.extractText()+ "\n"
+    block += text
+    count += 1
+    number_of_pages = read_pdf.numPages
+    if count==5:
+        print("Content Block ##\n", block, "\n")
+        hash = hashlib.sha256()
+        hash.update(block.encode('UTF-8'))
+        print("Content hash ##\n", hash.hexdigest() + "\n")
+        count = 0
+        block = ''
+
+    else:
+        print("Content Block ##\n", block, "\n")
+        hash = hashlib.sha256()
+        hash.update(block.encode('UTF-8'))
+        print("Content hash ##\n", hash.hexdigest() + "\n")
+
+
+
